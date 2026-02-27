3268263b0b7cd174d5a58311c579c813d4e60d8e
Emmanuel Valverde Ramos <evalverde@aidacanarias.com>
2026-02-27 13:40:32 +0100

    refactor: remove magic string, rename variable and remove unsessary visibility

--- ConsoleApp1/Game.cs
+++ ConsoleApp1/Game.cs
@@ -4,27 +4,28 @@ namespace ConsoleApp1;
 
 public class Game
 {
-    public int TargetNumber { get; set; }
+    private const int LimitOfAttmepts = 3;
+    private int targetNumber;
     private int attempts = 0;
 
     public Game(RandomNumberGenerator randomNumberGenerator)
     {
-        this.TargetNumber = randomNumberGenerator.Generate();
+        targetNumber = randomNumberGenerator.Generate();
     }
 
     public string Guess(int guessedNumber)
     {
         attempts++;
-        if (attempts == 3)
+        if (attempts == LimitOfAttmepts)
         {
             return "You lost";
         }
-        if (guessedNumber > TargetNumber)
+        if (guessedNumber > targetNumber)
         {
             return "Lower";
         }
 
-        if (guessedNumber < TargetNumber)
+        if (guessedNumber < targetNumber)
         {
             return "Higher";
         }
